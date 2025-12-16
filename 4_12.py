6ea15503cd66e68411de5a2ef2e60dea4468d353
Taisia <taechka.lu@rambler.ru>
2025-12-16 21:03:19 +0300

    Update 4_12.py
    Добавила фото и ссылки

--- 4_12.py
+++ 4_12.py
@@ -18,12 +18,24 @@ dp = Dispatcher()
 # Команды в Telegram
 async def set_commands(bot: Bot):
     commands = [
-        BotCommand(command="start", description="Запустить бота"),
-        BotCommand(command="help", description="Показать все команды"),
-        BotCommand(command="earth_photo", description="🌍 Фото Земли со спутников"),
-        BotCommand(command="apod", description="🛰 Фото дня от NASA (APOD)"),
-        BotCommand(command="planets", description="🪐 Справка о планетах"),
-        BotCommand(command="news", description="📰 Новости космоса"),
+        BotCommand(command="start",
+                   description="Запустить бота"
+        ),
+        BotCommand(command="help",
+                   description="Показать все команды"
+        ),
+        BotCommand(command="earth_photo",
+                   description="🌍 Фото Земли со спутника"
+        ),
+        BotCommand(command="apod",
+                   description="🛰 Фото дня от NASA (APOD)"
+        ),
+        BotCommand(command="planets",
+                   description="🪐 Справка о планетах"
+        ),
+        BotCommand(command="news",
+                   description="📰 Новости космоса"
+        ),
     ]
     await bot.set_my_commands(commands)
 
@@ -33,7 +45,7 @@ def get_menu():
     return InlineKeyboardMarkup(inline_keyboard=[
         [
             InlineKeyboardButton(
-                text='1 или /earth_photo - 🌍 Фото Земли со спутников',
+                text='1 или /earth_photo - 🌍 Фото Земли со спутника',
                 callback_data='photo of the earth'
             )
         ],
@@ -78,7 +90,9 @@ async def help(message: types.Message):
 @dp.message(Command("earth_photo"))
 async def earth_photo_cmd(message: types.Message):
     await message.answer_photo(
-        photo="https://resizer.mail.ru/p/a5db777f-57b6-56e2-a846-d28cb6add0f6/AQAKteqhd-KlJvH2QU-3mpvdd3E7LxmwXM0D8EpkGCZneW5xzAc7o3VbjvJgZQ_EcTfXrE0-3nFfEEon70v5Bwaf5DM.jpg",
+        photo="https://resizer.mail.ru/p/"
+              "a5db777f-57b6-56e2-a846-d28cb6add0f6/"
+              "AQAKteqhd-KlJvH2QU-3mpvdd3E7LxmwXM0D8EpkGCZneW5xzAc7o3VbjvJgZQ_EcTfXrE0-3nFfEEon70v5Bwaf5DM.jpg",
         caption="🌍 Это Земля из Космоса",
         reply_markup=get_menu()
     )
@@ -87,24 +101,30 @@ async def earth_photo_cmd(message: types.Message):
 @dp.message(Command("apod"))
 async def apod_cmd(message: types.Message):
     await message.answer_photo(
-        photo="https://apod.nasa.gov/apod/image/2508/Crab_HubbleChandraSpitzer_3600.jpg",
-        caption="🛰 Здесь будет фото дня от NASA",
+        photo="https://apod.nasa.gov/apod/image/"
+              "2508/Crab_HubbleChandraSpitzer_3600.jpg",
+        caption="🛰 Это фото дня от NASA",
         reply_markup = get_menu()
         )
 
 # Команда /planets
 @dp.message(Command("planets"))
 async def planets_cmd(message: types.Message):
-    await message.answer("🪐 Здесь будет справка о планетах",
-                         reply_markup = get_menu()
-                         )
+    await message.answer(
+        "🪐 Это справка о планетах\n"
+        "https://astrovert.ru/journal/solar_system/"
+        "planety-solnechnoy-sistemy-opisanie-klassifikatsiya-i-pravila-nablyudeniya/",
+        reply_markup = get_menu()
+    )
 
 # Команда /news
 @dp.message(Command("news"))
 async def news_cmd(message: types.Message):
-    await message.answer("📰 Здесь будут новости космоса",
-                         reply_markup = get_menu()
-                         )
+    await message.answer(
+        "📰 Это новости космоса\n"
+        "https:// lenta.ru/rubrics/science/cosmos/",
+        reply_markup = get_menu()
+    )
 
 
 # Обработка inline-кнопок
@@ -113,29 +133,36 @@ async def callback_message(callback: types.CallbackQuery):
 
     if callback.data == 'photo of the earth':
         await callback.message.answer_photo(
-            photo="https://resizer.mail.ru/p/a5db777f-57b6-56e2-a846-d28cb6add0f6/AQAKteqhd-KlJvH2QU-3mpvdd3E7LxmwXM0D8EpkGCZneW5xzAc7o3VbjvJgZQ_EcTfXrE0-3nFfEEon70v5Bwaf5DM.jpg",
+            photo="https://resizer.mail.ru/p/"
+                  "a5db777f-57b6-56e2-a846-d28cb6add0f6/"
+                  "AQAKteqhd-KlJvH2QU-3mpvdd3E7LxmwXM0D8EpkGCZneW5xzAc7o3VbjvJgZQ_EcTfXrE0-3nFfEEon70v5Bwaf5DM.jpg",
             caption="🌍 Это Земля из Космоса",
             reply_markup = get_menu()
-            )
+        )
 
     elif callback.data == 'photo of the day':
         await callback.message.answer_photo(
-            photo="https://apod.nasa.gov/apod/image/2508/Crab_HubbleChandraSpitzer_3600.jpg",
-             caption="🛰 Здесь будет фото дня от NASA",
+            photo="https://apod.nasa.gov/"
+                  "apod/image/2508/"
+                  "Crab_HubbleChandraSpitzer_3600.jpg",
+             caption="🛰 Это фото дня от NASA",
             reply_markup=get_menu()
         )
 
     elif callback.data == 'planetary reference':
         await callback.message.answer(
-            "🪐 Здесь будет справка о планетах",
+            "🪐 Это справка о планетах\n"
+            "https://astrovert.ru/journal/solar_system/"
+            "planety-solnechnoy-sistemy-opisanie-klassifikatsiya-i-pravila-nablyudeniya/",
             reply_markup = get_menu()
-            )
+        )
 
     elif callback.data == 'news':
         await callback.message.answer(
-            "📰 Здесь будут новости космоса",
+            "📰 Это новости космоса\n"
+            "https://lenta.ru/rubrics/science/cosmos/",
             reply_markup = get_menu()
-            )
+        )
 
     await callback.answer()
 
@@ -147,23 +174,36 @@ async def text_commands(message: types.Message):
 
     if text == "1":
         await message.answer_photo(
-            photo="https://resizer.mail.ru/p/a5db777f-57b6-56e2-a846-d28cb6add0f6/AQAKteqhd-KlJvH2QU-3mpvdd3E7LxmwXM0D8EpkGCZneW5xzAc7o3VbjvJgZQ_EcTfXrE0-3nFfEEon70v5Bwaf5DM.jpg",
+            photo="https://resizer.mail.ru/p/"
+                  "a5db777f-57b6-56e2-a846-d28cb6add0f6/"
+                  "AQAKteqhd-KlJvH2QU-3mpvdd3E7LxmwXM0D8EpkGCZneW5xzAc7o3VbjvJgZQ_EcTfXrE0-3nFfEEon70v5Bwaf5DM.jpg",
             caption="🌍 Это Земля из Космоса",
             reply_markup=get_menu()
         )
 
     elif text == "2":
         await message.answer_photo(
-            photo="https://apod.nasa.gov/apod/image/2508/Crab_HubbleChandraSpitzer_3600.jpg",
-            caption="🛰 Здесь будет фото дня от NASA",
+            photo="https://apod.nasa.gov/"
+                  "apod/image/2508/"
+                  "Crab_HubbleChandraSpitzer_3600.jpg",
+            caption="🛰 Это фото дня от NASA",
             reply_markup=get_menu()
         )
 
     elif text == "3":
-        await message.answer("🪐 Здесь будет справка о планетах", reply_markup=get_menu())
+        await message.answer(
+            "🪐 Это справка о планетах\n"
+            "https://astrovert.ru/journal/solar_system/"
+            "planety-solnechnoy-sistemy-opisanie-klassifikatsiya-i-pravila-nablyudeniya/",
+            reply_markup=get_menu()
+        )
 
     elif text == "4":
-        await message.answer("📰 Здесь будут новости космоса", reply_markup=get_menu())
+        await message.answer(
+            "📰 Это новости космоса\n"
+            "https://lenta.ru/rubrics/science/cosmos/",
+            reply_markup=get_menu()
+        )
 
     else:
         await message.answer(
